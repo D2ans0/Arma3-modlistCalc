1d6037aacfbb10a005956c90f8f26b409e63ec84
D2ans0 <git@d2ans0.tk>
2024-08-27 05:51:52 +0400

    add shell.nix for nixos users

---
+++ shell.nix
@@ -0,0 +1,13 @@
+with import <nixpkgs> {};
+
+let
+  pinned_hash = "f70001bf2db0d2f8b8072282685a0df2f5660ed7";
+  pinned_version = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/${pinned_hash}.tar.gz") { };
+in
+pkgs.mkShell {
+  buildInputs = with pinned_version; [
+    python312
+    python312Packages.beautifulsoup4
+    python312Packages.requests
+ ];
+}
